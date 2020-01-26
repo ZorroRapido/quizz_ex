91a1938559e26f21689479ef04596bfe13b53abe
ZorroRapido <56500870+ZorroRapido@users.noreply.github.com>
2020-01-26 14:54:44 +0300

    Competed 2 tasks

--- quiz.go
+++ quiz.go
@@ -28,15 +28,36 @@ func readCsv(filename string) []Question {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(records[0])
-	// Допишите код здесь
-	return nil
+	//fmt.Println(records[0])
+
+	questions := []Question{}
+
+	for _, i := range records {
+		questions = append(questions, Question{i[0], i[1]})
+	}
+
+	return questions
 }
 
 func main() {
 	total := 0
 
-	questions := []Question{{"1 + 1", "2"}, {"2 + 2", "4"}}
+	questions := readCsv("problems.csv")
+	//[]Question{{"1 + 1", "2"}, {"2 + 2", "4"}}
+
+	var answer string
+
+	for _, question := range questions {
+		fmt.Println(question.question)
+
+		fmt.Scan(&answer)
+
+		if answer == question.answer {
+			total++
+		}
+	}
+
+	fmt.Printf("You got %d/%d", total, len(questions))
 	// Пройтись циклом. Вывести вопрос, предложить пользователю ввести ответ.
 	// Если ответ правильный, увеличить total.
 	// for
